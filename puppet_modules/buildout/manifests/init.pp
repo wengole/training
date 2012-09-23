13f09328035dc5f9d2538e9c183140f76c4c4017
Philip Bauer <bauer@starzel.de>
2012-09-23 11:47:31 +0200

    fix owners and adapt path's for unified installer

--- puppet_modules/buildout/manifests/init.pp
+++ puppet_modules/buildout/manifests/init.pp
@@ -21,28 +21,22 @@ class buildout {
                'libbz2-dev',
                'wget',
                'curl',
-               'elinks',]:
+               'elinks',
+               'gettext']:
         ensure => installed,
-    }
-
-    file { '/etc/ssh/ssh_known_hosts':
-        ensure => present,
-        content => template('buildout/known_hosts'),
-        owner => 'root',
-        group => 'root',
-        mode => '644',
+        before => Exec["virtualenv"],
     }
 
     file { ['/home/vagrant/tmp',
             '/home/vagrant/.buildout',
-            '/home/vagrant/.buildout/buildout-cache',
-            '/home/vagrant/.buildout/buildout-cache/eggs',
-            '/home/vagrant/.buildout/buildout-cache/downloads',
-            '/home/vagrant/.buildout/buildout-cache/extends',]:
+            '/home/vagrant/buildout-cache',
+            '/home/vagrant/buildout-cache/eggs',
+            '/home/vagrant/buildout-cache/downloads',
+            '/home/vagrant/buildout-cache/extends',]:
         ensure => directory,
         owner => 'vagrant',
         group => 'vagrant',
-        mode => '0664',
+        mode => '0755',
     }
 
     file { '/home/vagrant/.buildout/default.cfg':
@@ -68,6 +62,8 @@ class buildout {
     exec {'wget https://launchpad.net/plone/4.2/4.2.1/+download/Plone-4.2.1-UnifiedInstaller.tgz':
         creates => '/home/vagrant/tmp/Plone-4.2.1-UnifiedInstaller.tgz',
         cwd => '/home/vagrant/tmp',
+        user => 'vagrant',
+        group => 'vagrant',
         before => Exec["untar1"],
     }
 
@@ -75,18 +71,24 @@ class buildout {
         alias => "untar1",
         creates => '/home/vagrant/tmp/Plone-4.2.1-UnifiedInstaller',
         cwd => '/home/vagrant/tmp',
+        user => 'vagrant',
+        group => 'vagrant',
         before => Exec["untar2"],
     }
 
-    exec {'tar -C /home/vagrant/.buildout/ -xjf buildout-cache.tar.bz2':
+    exec {'tar -C /home/vagrant/ -xjf buildout-cache.tar.bz2':
         alias => "untar2",
-        creates => '/home/vagrant/.buildout/buildout-cache/eggs/Products.CMFPlone-4.2.1.1-py2.7.egg',
+        creates => '/home/vagrant/buildout-cache/eggs/Products.CMFPlone-4.2.1.1-py2.7.egg',
+        user => 'vagrant',
+        group => 'vagrant',
         cwd => '/home/vagrant/tmp/Plone-4.2.1-UnifiedInstaller/packages/',
     }
 
     exec {'virtualenv --no-site-packages py27':
         alias => "virtualenv",
         creates => '/home/vagrant/py27',
+        user => 'vagrant',
+        group => 'vagrant',
         cwd => '/home/vagrant',
     }
 
